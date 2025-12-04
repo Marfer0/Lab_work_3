94e3a55b0045bc4d623e2a60c6df1ebae3bbe8e7
Marfer0 <vadimka.marunov@gmail.com>
2025-12-04 14:25:31 +0300

    фон окна + корректировка размеров

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
 mp = Tk()
-mp.geometry("1400x800+250+100")
+mp.geometry("800x540+250-100")
+mp.minsize(800,540)
 mp.title("Music Player")
-mp.iconbitmap(default='C:/Users/PC/Downloads/Lab_work_3/icon.ico')
+mp.iconbitmap(default='C:/Users/PC/PycharmProjects/Lab_work_3/icon.ico')
+mp['bg'] = '#363d4d'
 mp.mainloop()
